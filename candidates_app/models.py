94a09709f07ae93ec33999559e13d53741e2a17b
Moh <mohammed.dahiru72@gmail.com>
2024-03-18 11:57:30 +0100

    made migrations and updated requirements

--- candidates_app/models.py
+++ candidates_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from organisations_app.models import Organisation
 
 User = get_user_model()
 
@@ -9,7 +10,7 @@ class Candidate(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
-    organisation = models.ManyToManyField('Organisation', related_name='candidates')
+    organisation = models.ManyToManyField(Organisation, related_name='candidates')
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.first_name + ' ' + self.last_name
