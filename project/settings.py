5e6ddc80ce8d3a231dc78d1431eb2dc12d6cc202
Moh <mohammed.dahiru72@gmail.com>
2024-03-19 12:16:34 +0100

    db

--- project/settings.py
+++ project/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -102,9 +102,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # use database url if available in enviroment. else use one above
 
-DATABASES = {
-    'default': dj_database_url.parse(config('DATABASE_URL'))
-}
+# DATABASES = {
+#     'default': dj_database_url.parse(config('DATABASE_URL'))
+# }
 
 
 # Password validation
