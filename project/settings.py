aa9247b058c848369115775b56d7d1ca60a87a6a
Moh <mohammed.dahiru72@gmail.com>
2024-03-19 12:12:55 +0100

    db

--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -100,9 +102,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # use database url if available in enviroment. else use one above
 
-if config('DATABASE_URL'):
+if config('DATABASE_URL', default=None):
     DATABASES = {
-        'default': config('DATABASE_URL')
+        'default': dj_database_url.parse(config('DATABASE_URL'))
     }
 else:
     DATABASES = {
