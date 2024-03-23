d4ec0a8259c51026b4ff0f2ba18fbb57c74b7a6b
Moh <mohammed.dahiru72@gmail.com>
2024-03-23 16:12:36 +0100

    modified login endpoint

--- authentication/views.py
+++ authentication/views.py
@@ -28,8 +28,52 @@ class LoginView(generics.CreateAPIView):
             if not user.check_password(request.data['password']):
                 return Response({'error': 'Invalid password'}, status=status.HTTP_400_BAD_REQUEST)
             
+            # get organisation admins, super admins and reception staff attached to the user
+            organisation_admin = OrganisationAdmin.objects.filter(user=user)
+            super_admin = SuperAdmin.objects.filter(user=user)
+            reception_staff = ReceptionStaff.objects.filter(user=user)
+
+            # list of entities the user is attached to with ids, names and organisation ids
+            entities = {}
+
+            # organisation admins
+            organisation_admins = []
+            for org_admin in organisation_admin:
+                organisation_admins.append({
+                    'id': str(org_admin.id),
+                    'name': org_admin.first_name + ' ' + org_admin.last_name,
+                    'organisation_id': str(org_admin.organisation.id),
+                    'entity': 'organisation_admin',
+                    'organisation_name': org_admin.organisation.name
+                })
+            
+            # super admins
+            super_admins = []
+            for s_admin in super_admin:
+                super_admins.append({
+                    'id': str(s_admin.id),
+                    'name': s_admin.first_name + ' ' + s_admin.last_name,
+                    'entity': 'super_admin'
+                })
+            
+            # reception staff
+            reception_staffs = []
+            for r_staff in reception_staff:
+                reception_staffs.append({
+                    'id': str(r_staff.id),
+                    'name': r_staff.first_name + ' ' + r_staff.last_name,
+                    'entity': 'reception_staff'
+                })
+
+            entities['organisation_admins'] = organisation_admins
+            entities['super_admins'] = super_admins
+            entities['reception_staffs'] = reception_staffs
+
+
+            
+            
             token, created = TimedAuthToken.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
+            return Response({'token': token.key, 'entities':entities}, status=status.HTTP_200_OK)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
