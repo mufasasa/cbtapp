a53c469af9fedd7764f6fd6310aefbbb725b8e64
Moh <mohammed.dahiru72@gmail.com>
2024-04-16 19:09:15 +0100

    bugg fix

--- organisations_app/urls.py
+++ organisations_app/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
 
     path('organisation/complains/', OrganisationComplainListCreateView.as_view(), name='organisation_complains'),
     path('organisation/complains/<uuid:complain_id>/', OrganisationComplainDetailView.as_view(), name='organisation_complain_detail'),
-    path('exam/<uuid:exam_id/candidates/', RetreiveExamCandidatesView.as_view(), name='exam_candidates'),
+    path('exam/<uuid:exam_id>/candidates/', RetreiveExamCandidatesView.as_view(), name='exam_candidates'),
 
 ]
