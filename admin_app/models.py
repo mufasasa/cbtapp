94934d65cfb7fe8362f7f98fd011156fb262323b
Moh <mohammed.dahiru72@gmail.com>
2024-05-03 15:08:23 +0100

    modified visistor

--- admin_app/models.py
+++ admin_app/models.py
@@ -35,11 +35,9 @@ class ReceptionStaff(models.Model):
 
 class Visitor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=100, blank=True, null=True)
+    name = models.CharField(max_length=100)
     purpose = models.TextField()
+    who_to_visit = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return self.name
