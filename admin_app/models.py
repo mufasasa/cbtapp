378467c51a0894885cd13e1e526922125e1a44a8
Moh <mohammed.dahiru72@gmail.com>
2024-05-06 15:30:20 +0100

    modified model  name

--- admin_app/models.py
+++ admin_app/models.py
@@ -37,7 +37,7 @@ class Visitor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     purpose = models.TextField()
-    who_to_visit = models.CharField(max_length=100, blank=True, null=True)
+    whom_to_visit = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
